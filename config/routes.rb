6c8decb4e6e0f17ce4d1f2d975465a6ba6792f5e
derrick b <dbozich1@gmail.com>
2018-08-31 01:58:42 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :sessions
     resources :secrets 
+    post '/sessions' => 'sessions#destroy'
 
   # Example resource route with options:
   #   resources :products do
