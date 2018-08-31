09fff6f9f9462fea659b0bc2e214e4613a376f25
derrick b <dbozich1@gmail.com>
2018-08-31 02:37:17 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,9 @@
 class SessionsController < ApplicationController
   def create
     session[:name] = params[:name] if params[:name].present?
-
-
     if current_user.nil?
       redirect_to new_session_path
     else
-
-
       redirect_to user_path
     end
 
